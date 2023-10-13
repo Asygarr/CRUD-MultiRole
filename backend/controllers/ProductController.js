afc8f08b1206392df1fb4d435172a75dc5745460
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-13 17:54:48 +0800

    initial commit backend

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,11 @@
+import Product from "../models/ProductModel.js";
+
+export const getProduct = async (req, res) => {};
+
+export const getProductsById = async (req, res) => {};
+
+export const createProduct = async (req, res) => {};
+
+export const updateProduct = async (req, res) => {};
+
+export const deleteProduct = async (req, res) => {};
---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  getUser,
+  getUsersById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get("/users", getUser);
+router.get("/users/:id", getUsersById);
+router.post("/users", createUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+
+export default router;
