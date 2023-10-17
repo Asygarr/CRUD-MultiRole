2f7c476b9269b71e2a308aae55a8d59eb3a23ce9
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-17 22:16:17 +0800

    Menyelesaikan Product controller

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,11 +1,173 @@
 import Product from "../models/ProductModel.js";
+import User from "../models/UserModel.js";
+import { Op } from "sequelize";
 
-export const getProduct = async (req, res) => {};
+export const getProduct = async (req, res) => {
+  try {
+    let response;
+    if (req.role === "admin") {
+      response = await Product.findAll({
+        attributes: ["uuid", "name", "price"],
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    } else {
+      response = await Product.findAll({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          userId: req.userId,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const getProductsById = async (req, res) => {};
+export const getProductsById = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
 
-export const createProduct = async (req, res) => {};
+    if (!product) return res.status(404).json({ message: "Product not found" });
 
-export const updateProduct = async (req, res) => {};
+    let response;
+    if (req.role === "admin") {
+      response = await Product.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          id: product.id,
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    } else {
+      response = await Product.findOne({
+        attributes: ["uuid", "name", "price"],
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+        include: [
+          {
+            model: User,
+            attributes: ["name", "email"],
+          },
+        ],
+      });
+    }
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const deleteProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  const { name, price } = req.body;
+
+  try {
+    await Product.create({
+      name,
+      price,
+      userId: req.userId,
+    });
+    res.status(201).json({ message: "Product created" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Product.update(
+        {
+          name,
+          price,
+        },
+        {
+          where: {
+            id: product.id,
+          },
+        }
+      );
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ message: "Akses Terlarang" });
+
+      await Product.update(
+        {
+          name,
+          price,
+        },
+        {
+          where: {
+            [Op.and]: [{ id: product.id }, { userId: req.userId }],
+          },
+        }
+      );
+    }
+    res.status(200).json({ message: "Product telah di update" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findOne({
+      where: {
+        uuid: req.params.id,
+      },
+    });
+
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    const { name, price } = req.body;
+    if (req.role === "admin") {
+      await Product.destroy({
+        where: {
+          id: product.id,
+        },
+      });
+    } else {
+      if (req.userId !== product.userId)
+        return res.status(403).json({ message: "Akses Terlarang" });
+
+      await Product.destroy({
+        where: {
+          [Op.and]: [{ id: product.id }, { userId: req.userId }],
+        },
+      });
+    }
+    res.status(200).json({ message: "Product telah di hapus" });
+  } catch (error) {
+    console.log(error);
+  }
+};
