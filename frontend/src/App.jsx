8b4f47a4f0319e7c0a4af430536c32eb0ee494e6
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-28 20:26:09 +0800

    penyesuain auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Users from "./pages/users/Users";
-import Products from "./pages/products/Products";
 import AddUser from "./pages/users/AddUser";
 import EditUser from "./pages/users/EditUser";
+import Products from "./pages/products/Products";
 import AddProduct from "./pages/products/AddProduct";
 import EditProduct from "./pages/products/EditProduct";
 
