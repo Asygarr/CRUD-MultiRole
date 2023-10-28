e2721ac8565cf22e235dcf8f499734c02af7c13b
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-28 21:57:39 +0800

    penyesuaian

--- frontend/src/contents/Welcome.jsx
+++ frontend/src/contents/Welcome.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Welcome = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <div>
       <h1 className="title">Dashboard</h1>
-      <h2 className="subtitle">Welcome Back ...</h2>
+      <h2 className="subtitle">
+        Welcome Back <strong>{user && user.name}</strong>
+      </h2>
     </div>
   );
 };
