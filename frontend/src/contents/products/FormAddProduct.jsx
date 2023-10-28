301775c3e3e68173d892aa9f531bac82eb1ce06c
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-28 21:58:23 +0800

    fetch data product

--- frontend/src/contents/products/FormAddProduct.jsx
+++ frontend/src/contents/products/FormAddProduct.jsx
@@ -1,7 +1,30 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const FormAddProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [massage, setMassage] = useState("");
+
+  const navigate = useNavigate();
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/products", {
+        name,
+        price,
+      });
+      navigate("/products");
+    } catch (error) {
+      if (error.response) {
+        setMassage(error.response.data.message);
+      }
+    }
+  };
+
   return (
     <div>
       <h1 className="title">Product</h1>
@@ -10,24 +33,39 @@ const FormAddProduct = () => {
       <div className="card is-shadowless">
         <div className="card-content">
           <div className="content">
-            <form>
+            <form onSubmit={saveProduct}>
+              <p className="has-text-centered">{massage}</p>
               <div className="field">
                 <label className="label">Product Name</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="name" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="name"
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <label className="label">Price</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="price" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    placeholder="price"
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <div className="control">
-                  <button className="button is-success">Save</button>
+                  <button type="submit" className="button is-success">
+                    Save
+                  </button>
                 </div>
               </div>
             </form>
--- frontend/src/contents/products/FormEditProduct.jsx
+++ frontend/src/contents/products/FormEditProduct.jsx
@@ -1,7 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 const FormEditProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [message, setMessage] = useState("");
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getProductById = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/products/${id}`
+        );
+        setName(response.data.name);
+        setPrice(response.data.price);
+      } catch (error) {
+        if (error.response) {
+          setMessage(error.response.data.message);
+        }
+      }
+    };
+    getProductById();
+  }, [id]);
+
+  const editProduct = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.patch(`http:localhost:5000/products/${id}`, {
+        name,
+        price,
+      });
+      navigate("/products");
+    } catch (error) {
+      if (error.response) {
+        setMessage(error.response.data.message);
+      }
+    }
+  };
+
   return (
     <div>
       <h1 className="title">Product</h1>
@@ -10,24 +51,39 @@ const FormEditProduct = () => {
       <div className="card is-shadowless">
         <div className="card-content">
           <div className="content">
-            <form>
+            <form onSubmit={editProduct}>
+              <p className="has-text-centered">{message}</p>
               <div className="field">
                 <label className="label">Product Name</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="name" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="name"
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <label className="label">Price</label>
                 <div className="control">
-                  <input type="text" className="input" placeholder="price" />
+                  <input
+                    type="text"
+                    className="input"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    placeholder="price"
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <div className="control">
-                  <button className="button is-success">Update</button>
+                  <button type="submit" className="button is-success">
+                    Update
+                  </button>
                 </div>
               </div>
             </form>
