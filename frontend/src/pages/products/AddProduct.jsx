35b576de315a56c6f301180c774fac2e3f7e2424
Asygarr <105841104721@student.unismuh.ac.id>
2023-10-28 20:27:18 +0800

    penyelesaian konten akses

--- frontend/src/pages/products/AddProduct.jsx
+++ frontend/src/pages/products/AddProduct.jsx
@@ -1,7 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
 import Layout from "../../layouts/Main";
 import FormAddProduct from "../../contents/products/FormAddProduct";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getMe } from "../auth/authSlice";
 
 const AddProduct = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isError } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/");
+    }
+  }, [isError, navigate]);
+
   return (
     <Layout>
       <FormAddProduct />
--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -1,9 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../../layouts/Main";
 import ProductList from "../../contents/products/ProductList";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getMe } from "../auth/authSlice";
 
 const Products = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isError } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isError) {
+      navigate("/");
+    }
+  }, [isError, navigate]);
+
   return (
     <div>
       <Layout>
